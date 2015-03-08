e2f82bf1769b073d5b2caead57098fc5cd720675
Pine Mizune <pinemz@gmail.com>
2015-03-09 07:49:25 +0900

    Fix files type

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     mochacov: {
       options: {
-        files: 'test/**/*-test.js',
+        files: ['test/**/*-test.js'],
         timeout: 10 * 1000
       },
       test: {
